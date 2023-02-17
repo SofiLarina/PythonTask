3957cf7e09e28e8098b914b18e5a8149c67768c6
unknown <sofi.larina2007@yandex.ru>
2023-02-17 12:34:31 +0300

    Lesson 5(tasks 1-6), lesson 6(tasks 1-5)

--- HomeWork/lesson5/l5_t5.py
+++ HomeWork/lesson5/l5_t5.py
@@ -1,8 +1,6 @@
-a = list(int(input('Введите число:'))).split()
-if (a % 3 == 0) and (a[-1] % 2 == 0):
-    print('Число делится на 6')
+num = int(input('Введите число:'))
+if (num%10)%2==0:
+    if (sum(map(int, str(num))))%3 == 0:
+        print('Число делится на 6')
 else:
     print('Число не делится на 6')
-
-number = int(input('Введите число:'))
-if
---
+++ HomeWork/lesson6/l6_t6.py
@@ -0,0 +1,11 @@
+summa = int(input('Введите сумму к оплате:'))
+while summa % 2 == 0:
+    summa = summa / 2
+print('К оплате:', summa)
+
+# summa = int(input('Введите сумму к оплате:'))
+# if summa % 2 == 0:
+#     print('К оплате:', summa / 2)
+# else:
+#     print('К оплате:', summa * 0.15)
+
