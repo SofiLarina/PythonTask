0e35ba3a3b6b3361bfaed118a41ca408575e55c7
unknown <sofi.larina2007@yandex.ru>
2023-03-15 13:19:29 +0300

    Lesson 11 Task 1-5

--- HomeWork/lesson9/l9_t3.py
+++ HomeWork/lesson9/l9_t3.py
@@ -4,9 +4,10 @@
 Даны списки семей в квартирах.
 Используя операции со множествами вычислите cколько семей живёт в доме N.
 """
-newspaper = set(range(1, 76))
-magazine = set(range(77, 104))
-both = set(range(21, 34))
-h = (magazine - both)
-b = len((newspaper | h))
-print(list(h))
+newspaper = range(1, 76)
+magazine = range(77, 104)
+both = range(21, 34)
+
+join = max(set(newspaper).union(set(magazine))) - min(set(newspaper).union(set(magazine)))+1
+print(join - (max(both) - min(both) + 1))
+
