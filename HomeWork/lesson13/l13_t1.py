4fe088c26a0f4389edb7200f01428619889fbddd
SofiLarina <sofi.larina2007@yandex.ru>
2023-03-21 10:27:51 +0300

    Lesson13 Tasks

--- HomeWork/lesson13/l13_t1.py
+++ HomeWork/lesson13/l13_t1.py
@@ -5,3 +5,35 @@
 Если 30 минут закончились или игрок вводит «off» — завершать работу.
 Оформить в виде функции.
 """
+from time import time
+def chess():
+    start = time()
+    remaining = 30
+    while remaining > 0:
+        move = input("Введите свой ход: ")
+        if move == "off":
+            break
+        elapsed = int(time() - start) // 60
+        remaining = 30 - elapsed
+        print(f"Осталось времени: {remaining} минут")
+    print("Игра завершена")
+
+chess()
+
+"""
+from time import time
+
+def chess():
+    remaining = 30
+    move = input("Введите свой ход: ")
+    start = time()
+    while remaining > 0 and move != "off":
+        move = input("Введите свой ход: ")
+        end = time()
+        move_time = end - start
+        left = left - move_time
+        print(f"Осталось времени: {left}")
+chess()
+"""
+
+
