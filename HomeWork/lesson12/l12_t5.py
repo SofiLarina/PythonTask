da8d26954af338dcd61f8fd54dfae1efe81f8590
SofiLarina <sofi.larina2007@yandex.ru>
2023-03-21 09:06:03 +0300

    Lesson 12 Tasks 1-5

--- HomeWork/lesson12/l12_t5.py
+++ HomeWork/lesson12/l12_t5.py
@@ -3,4 +3,22 @@
 Напишите программу, принимающую 2 позиционных аргумента: слово "Цвета" и количество цветов
 и произвольное количество значений Цвет: HEX. Программа должна вывести все данные на экран.
 """
+import random
 
+def generate_hex_colors(color_word, num_colors):
+    hex_values = '0123456789ABCDEF'
+    colors = {}
+    for i in range(int(num_colors)):
+        color = ''
+        for j in range(6):
+            color += random.choice(hex_values)
+        colors[f"{color_word} {i+1}"] = f"#{color}"
+    return colors
+
+color_word = input("Введите слово для обозначения цветов: ")
+num_colors = input("Введите количество цветов: ")
+
+colors = generate_hex_colors(color_word, num_colors)
+
+for color, hex_value in colors.items():
+    print(f"{color}: {hex_value}")
